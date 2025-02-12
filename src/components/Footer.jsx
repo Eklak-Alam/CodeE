7bc2436f83d82ecaf0287b80e273bd5e1629437f
Eklak Alam <eklakalam420@gmail.com>
2025-02-12 13:08:09 +0530

    Added project files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaInstagram, FaLinkedin, FaGithub, FaYoutube } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
@@ -21,10 +21,10 @@ const Footer = () => {
           <div>
             <h3 className="text-xl font-semibold mb-3">Quick Links</h3>
             <ul className="space-y-2">
-              <li><Link href="/" className="text-gray-400 hover:text-white">Home</Link></li>
-              <li><Link href="/about" className="text-gray-400 hover:text-white">About</Link></li>
-              <li><Link href="/course-list" className="text-gray-400 hover:text-white">Courses</Link></li>
-              <li><Link href="/contact" className="text-gray-400 hover:text-white">Contact</Link></li>
+              <li><Link to="/" className="text-gray-400 hover:text-white">Home</Link></li>
+              <li><Link to="/about" className="text-gray-400 hover:text-white">About</Link></li>
+              <li><Link to="/course-list" className="text-gray-400 hover:text-white">Courses</Link></li>
+              <li><Link to="/contact" className="text-gray-400 hover:text-white">Contact</Link></li>
             </ul>
           </div>
 
@@ -32,10 +32,10 @@ const Footer = () => {
           <div>
             <h3 className="text-xl font-semibold mb-3">Follow Us</h3>
             <div className="flex justify-center md:justify-start space-x-4">
-              <a href="/" className="text-gray-400 hover:text-blue-500"><FaFacebook size={24} /></a>
-              <a href="/" className="text-gray-400 hover:text-blue-400"><FaTwitter size={24} /></a>
-              <a href="/" className="text-gray-400 hover:text-pink-500"><FaInstagram size={24} /></a>
-              <a href="/" className="text-gray-400 hover:text-blue-600"><FaLinkedin size={24} /></a>
+              <Link target="_blank" to="https://www.linkedin.com/in/eklak-alam-40ba632b5/" className="text-gray-400 hover:text-blue-500"><FaLinkedin size={24} /></Link>
+              <Link target="_blank" to="https://github.com/Eklak-Alam" className="text-gray-400 hover:text-gray-500"><FaGithub size={24} /></Link>
+              <Link target="_blank" to="https://www.instagram.com/heartless_eklak/" className="text-gray-400 hover:text-pink-500"><FaInstagram size={24} /></Link>
+              <Link target="_blank" to="https://www.youtube.com/@eklakalam04" className="text-gray-400 hover:text-red-700"><FaYoutube size={24} /></Link>
             </div>
           </div>
 
